ae5e7a9a245a746a9c358474f2013e7265370283
thebenjieangeles <thebenjangeles@gmail.com>
2025-08-30 23:20:54 +0800

    added images for each drum buttons

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrums; i++)
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
   });
